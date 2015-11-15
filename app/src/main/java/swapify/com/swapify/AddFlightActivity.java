5078f64d173bd559480010857856af4bce862693
Ria Jiang <riarunjiejiang@gmail.com>
2015-11-15 15:17:53 +0100

    Get flight info from editText fields and saves it in background

--- app/src/main/java/swapify/com/swapify/AddFlightActivity.java
+++ app/src/main/java/swapify/com/swapify/AddFlightActivity.java
@@ -3,6 +3,10 @@ package swapify.com.swapify;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.SaveCallback;
 
 public class AddFlightActivity extends Activity {
 
@@ -14,6 +18,23 @@ public class AddFlightActivity extends Activity {
         findViewById(R.id.save_flight_info).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText flightNoText = (EditText) findViewById(R.id.edit_flight_no);
+                String flightNo = flightNoText.getText().toString();
+                EditText planeTypeText = (EditText) findViewById(R.id.edit_plane_type);
+                String planeType = planeTypeText.getText().toString();
+                EditText seatNoText = (EditText) findViewById(R.id.edit_seat_no);
+                String seatNo = seatNoText.getText().toString();
+
+                FlightInfo flightInfo = new FlightInfo();
+                flightInfo.setFlightNo(flightNo);
+                flightInfo.setPlaneType(planeType);
+                flightInfo.setSeatNo(seatNo);
+                flightInfo.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        
+                    }
+                });
             }
         });
     }
