b175c558c4564b7d51db2f29c35138c092454357
Ria Jiang <riarunjiejiang@gmail.com>
2015-11-15 15:01:55 +0100

    Add click listener for save flight button

--- app/src/main/java/swapify/com/swapify/AddFlightActivity.java
+++ app/src/main/java/swapify/com/swapify/AddFlightActivity.java
@@ -2,6 +2,7 @@ package swapify.com.swapify;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddFlightActivity extends Activity {
 
@@ -9,5 +10,11 @@ public class AddFlightActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_flight);
+
+        findViewById(R.id.save_flight_info).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });g
     }
 }
