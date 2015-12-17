2c94ac5965dbfe8e007343a4b9becb66c0687141
Ria Jiang <riarunjiejiang@gmail.com>
2015-12-17 10:02:35 +0100

    Change app id and key

--- app/src/main/java/swapify/com/swapify/Constants.java
+++ app/src/main/java/swapify/com/swapify/Constants.java
@@ -1,6 +1,6 @@
 package swapify.com.swapify;
 
 public class Constants {
-    public static final String Flight_Stats_App_Id = "7fecd6cb";
-    public static final String Flight_Stats_API_Key = "8fa43f653c8fefa3b4c66eb2e28060da";
+    public static final String Flight_Stats_App_Id = "767cfa4d";
+    public static final String Flight_Stats_API_Key = "3d3844129a5cbb1198e11de4a4eff5cc";
 }
